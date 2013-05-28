7a92309e0691886e4b0330112bf4ca3d8c4366c3
john irving <whimful@gmail.com>
2013-05-28 14:51:27 +1200

    create simple_form

--- app/controllers/membership_requests_controller.rb
+++ app/controllers/membership_requests_controller.rb
@@ -1,6 +1,7 @@
 class MembershipRequestsController < ApplicationController
   def new
     @group = Group.find(params[:group_id])
+    @membership_request = MembershipRequest.new#(group: @group)
   end
 
   def create
